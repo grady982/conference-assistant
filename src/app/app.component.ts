51fdafcc62f5e52c45188eb50ea82fcebd305152
grady.liu <grady.liu@webcomm.com.tw>
2024-05-13 09:46:43 +0800

    chore: Add Firebase configuration files and update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoadingService } from './core/loading.service';
 import { OpenaiService } from './core/openai.service';
+import { mockSpeechToText, mockSummary } from './core/mockData';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +16,9 @@ export class AppComponent implements OnInit {
   fileName: string = 'choose a file...';
   speechToText: string = '';
   summary: string = '';
+
+  mockSpeechToText = mockSpeechToText;
+  mockSummary = mockSummary;
 
   constructor(
     private loading: LoadingService,
@@ -32,16 +36,22 @@ export class AppComponent implements OnInit {
 
     this.fileName = event.target.files[0].name;
 
+    setTimeout(() => {  
+      this.speechToText = this.mockSpeechToText;
+      this.summary = this.mockSummary;
+      this.loading.close();
+    } , 3000);
+
     // --
-    const file = event.target.files[0];
-    console.log('file', file);
-    this.openai.transcribeAudio(file).subscribe(response => {
-      this.loading.close();
-      // this.speechToText = response.choices[0].text;
-    }, (error) => {
-      this.loading.close();
-      console.warn('error', error);
-    });
+    // const file = event.target.files[0];
+    // console.log('file', file);
+    // this.openai.transcribeAudio(file).subscribe(response => {
+    //   this.loading.close();
+    //   // this.speechToText = response.choices[0].text;
+    // }, (error) => {
+    //   this.loading.close();
+    //   console.warn('error', error);
+    // });
 
   }
 
